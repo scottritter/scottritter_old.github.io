96a804b79a61e377e0b92f10d2754020ec922a7f
charlieritter <sirlierach@gmail.com>
2015-03-30 10:39:14 -0400

    adds slideshow.js, removes some old files

---
+++ scripts/slideshow.js
@@ -0,0 +1,14 @@
+<!-- hide script from old browsers
+
+		$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+end hiding script from old browswers-->
